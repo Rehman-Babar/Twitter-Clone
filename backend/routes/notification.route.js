6b20d9dc12941ca1cfd23b3e1a8fef821f36ec47
Rehman-Babar327 <153273576+Rehman-Babar@users.noreply.github.com>
2024-07-02 02:24:50 -0700

    tailwind and daisy Ui entigrated

--- backend/routes/notification.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {protectRoutes} from '../middleware/protectRoutes.js'
-import { DeleteNotification, GetNotification } from '../controller/notification.controller.js'
-const router = express.Router()
-
-router.get("/", protectRoutes, GetNotification)
-router.delete("/", protectRoutes, DeleteNotification)
-
-export default router
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+    <p className='text-red-400 bg-blue-500'>hisduiyfcgv</p>
+    <button class="btn btn-primary">Button</button>
+    </>
+  );
+}
+
+export default App;

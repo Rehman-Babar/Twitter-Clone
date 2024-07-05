7dafba463d52c5addd0f3ec2ca0cc23d31d0f124
Rehman-Babar327 <153273576+Rehman-Babar@users.noreply.github.com>
2024-07-04 22:10:43 -0700

    UI-Design Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignupPage from "./pages/auth/signup/SignupPage";
+import HomePage from "./pages/home/HomePage";
+import Sidebar from "./component/common/SideBar";
+import RightPanel from "./component/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
 function App() {
   return (
-    <>
-    <p className='text-red-400 bg-blue-500'>hisduiyfcgv</p>
-    <button class="btn btn-primary">Button</button>
-    </>
+    <div className='flex max-w-6xl mx-auto'>
+
+      <Sidebar/>
+      <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/signup' element={<SignupPage />} />
+          <Route path='/login'  element={ <LoginPage />} />
+          <Route path='/notifications'  element={ <NotificationPage />} />
+          <Route path='/profile/:userName'  element={ <ProfilePage />} />
+			</Routes>
+      <RightPanel/>
+    </div>
   );
 }
 

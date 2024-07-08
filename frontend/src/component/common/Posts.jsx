3e76405beee6f5169538404db7a7df521be7beb2
Rehman-Babar327 <153273576+Rehman-Babar@users.noreply.github.com>
2024-07-07 20:38:13 -0700

    3- fetch posts for the home page

--- frontend/src/component/common/Posts.jsx
+++ frontend/src/component/common/Posts.jsx
@@ -1,23 +1,58 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy.js";
+// import { POSTS } from "../../utils/db/dummy.js";
+import { useQuery } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useEffect } from "react";
 
-const Posts = () => {
-	const isLoading = false;
 
+const Posts = ({feedType}) => {
+
+	const getPostsEndPoints = () => {
+		switch(feedType) {
+			case "foryou" :
+				return "/api/posts/all";
+			case "following":
+				return "/api/posts/following";
+			default :
+				return "/api/posts/all";
+		}
+	}
+
+	const PostsEndPoints = getPostsEndPoints()
+	console.log(PostsEndPoints)
+	const {data:posts, isLoading, refetch, isFetching} = useQuery({
+		queryKey:["posts"],
+		queryFn:async () => {
+			try {
+				const res = await fetch(PostsEndPoints)
+				const data = await res.json()
+				if (data.error) {
+					toast.error(data.error.message)
+				}
+				console.log("posts",data)
+				return data
+			} catch (error) {
+				throw new Error(error)
+			}
+		}
+	})
+	useEffect(() => {
+		refetch();
+	}, [feedType])
 	return (
 		<>
-			{isLoading && (
+			{(isLoading || isFetching ) && (
 				<div className='flex flex-col justify-center'>
 					<PostSkeleton />
 					<PostSkeleton />
 					<PostSkeleton />
 				</div>
 			)}
-			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && POSTS && (
+			{!isLoading && isFetching && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isLoading && posts && (
 				<div>
-					{POSTS.map((post) => (
+					{posts.map((post) => (
 						<Post key={post._id} post={post} />
 					))}
 				</div>

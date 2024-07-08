ba66696f024765863308bb361d75e4550ed8fb2b
Rehman-Babar327 <153273576+Rehman-Babar@users.noreply.github.com>
2024-07-08 05:09:35 -0700

    PayLoad limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import {v2 as cloudinary} from 'cloudinary'
 import cors from 'cors'
 
 import authRoutes from './routes/auth.route.js';
-import userRoutes from './routes/userroute.js';
+import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.routes.js';
 import notificationRoutes from './routes/notification.route.js';
 

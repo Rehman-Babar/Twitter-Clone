bc43dec59f3a19b8c00120b2ad8e7bbe49667e81
Rehman-Babar327 <153273576+Rehman-Babar@users.noreply.github.com>
2024-07-08 05:44:57 -0700

    Suggested users fetched

--- frontend/src/component/common/RightPanel.jsx
+++ frontend/src/component/common/RightPanel.jsx
@@ -1,11 +1,25 @@
 import { Link } from "react-router-dom";
-// import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
+import { useQuery } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const RightPanel = () => {
-	const isLoading = false;
-
+	const {data:suggestedUser, isLoading} =useQuery({
+		queryKey:["suggestedUser"],
+		queryFn:async() => {
+			try {
+				const res =await fetch('/api/users/suggested')
+				const data =await res.json();
+				if (data.error) {
+					return toast.error(data.error.message)
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error)
+			}
+		}
+	})
+	if (suggestedUser?.length === 0) return <div className="md:w-64 sm:w-0"></div>
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
 			<div className='bg-[#16181C] p-4 rounded-md sticky top-2'>
@@ -21,7 +35,7 @@ const RightPanel = () => {
 						</>
 					)}
 					{!isLoading &&
-						USERS_FOR_RIGHT_PANEL?.map((user) => (
+						suggestedUser?.map((user) => (
 							<Link
 								to={`/profile/${user.username}`}
 								className='flex items-center justify-between gap-4'
